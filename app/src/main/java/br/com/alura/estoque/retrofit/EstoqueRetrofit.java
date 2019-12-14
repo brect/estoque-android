52c7b1d19f00c11f4bb5eaeb6c7601c438cf61d8
brect <b.limas93@gmail.com>
2019-12-13 23:22:12 -0300

    Adicionar e configurar o Retrofit; Implementar e executar requisições HTTP; Verificar informações trafegadas nas requisições HTTP.

--- app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -1,12 +1,33 @@
 package br.com.alura.estoque.retrofit;
 
+import br.com.alura.estoque.model.Produto;
+import br.com.alura.estoque.retrofit.service.ProdutoService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EstoqueRetrofit {
 
+    private final ProdutoService produtoService;
+
     public EstoqueRetrofit() {
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.15.39:8080/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        produtoService = retrofit.create(ProdutoService.class);
+    }
+
+    public ProdutoService getProdutoService() {
+        return produtoService;
     }
 }
